30b5e7ac9377b4baecacc4c55127e191b0ae70be
Didier Raboud <me+defivelo@odyx.org>
2015-08-27 11:09:55 +0200

    Fix the urls's isort syntax

--- defivelo/urls.py
+++ defivelo/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
