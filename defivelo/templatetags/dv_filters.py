3fe60a8bd53ed6ee7017d8ec3ce8e761783362f0
Didier Raboud <me+defivelo@odyx.org>
2015-10-16 11:59:47 +0200

    Applique la license AGPLv3 sur tous les fichiers *.py

--- defivelo/templatetags/dv_filters.py
+++ defivelo/templatetags/dv_filters.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+#
+# defivelo-intranet -- Outil métier pour la gestion du Défi Vélo
+# Copyright (C) 2015 Didier Raboud <me+defivelo@odyx.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import unicode_literals
 
 from re import search, sub
