c6e73415e7926fc230a9294efff41915f037e019
Didier Raboud <me+defivelo@odyx.org>
2016-08-31 18:28:10 +0200

    Fix wsgi import_env_vars call

--- defivelo/wsgi.py
+++ defivelo/wsgi.py
@@ -30,7 +30,7 @@ from django.core.wsgi import get_wsgi_application
 
 from defivelo import get_project_root_path, import_env_vars
 
-import_env_vars(os.path.join(get_project_root_path(), 'envdir', 'local'))
+import_env_vars(os.path.join(get_project_root_path(), 'envdir'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "defivelo.settings.base")
 
