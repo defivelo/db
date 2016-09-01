e1da47d79f7288a0c27716790dfb37ebdcc632ca
Didier Raboud <me+defivelo@odyx.org>
2016-09-01 15:44:53 +0000

    Add missing migration

---
+++ apps/user/migrations/0014_auto_20160901_1744.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import localflavor.generic.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user', '0013_auto_20151015_1824'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userprofile',
+            name='iban',
+            field=localflavor.generic.models.IBANField(False, ('AT', 'BE', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 'GB', 'GI', 'GR', 'HR', 'HU', 'IE', 'IS', 'IT', 'LI', 'LT', 'LU', 'LV', 'MC', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'SM'), blank=True),
+        ),
+    ]
