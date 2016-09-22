4fbf25d61b19e4eef6ae910dc2ef9308ff9c3e8c
Didier Raboud <me+defivelo@odyx.org>
2016-09-22 15:44:36 +0000

    Need unicode literals for the crown
    Closes: #17

--- apps/challenge/__init__.py
+++ apps/challenge/__init__.py
@@ -15,6 +15,8 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import unicode_literals
+
 AVAILABILITY_FIELDKEY_HELPER_PREFIX = 'avail-h{hpk}-'
 STAFF_FIELDKEY_HELPER_PREFIX = 'staff-h{hpk}-'
 
