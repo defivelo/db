d53ebd53843672eb1e53402c75a00cba884d791e
Didier Raboud <me+defivelo@odyx.org>
2016-09-29 14:03:05 +0200

    Drop unused django-jenkins

--- defivelo/settings/test.py
+++ defivelo/settings/test.py
@@ -24,18 +24,3 @@ SECRET_KEY = 'notsosecret'
 NEVERCACHE_KEY = 'notsosecret'
 
 INSTALLED_APPS = (('test_without_migrations', ) + INSTALLED_APPS)
-INSTALLED_APPS += (  # NOQA
-    'django_jenkins',
-)
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.with_coverage',
-    # 'django_jenkins.tasks.run_jshint',
-    # 'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_flake8',
-    # 'django_jenkins.tasks.run_sloccount',
-    # 'django_jenkins.tasks.run_graphmodels',
-)
