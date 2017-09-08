38c9f412f901c36446f967ad8390349293da1a47
Didier Raboud <me+defivelo@odyx.org>
2017-09-08 08:12:16 +0000

    Flake8

--- defivelo/views/__init__.py
+++ defivelo/views/__init__.py
@@ -17,4 +17,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import unicode_literals
 
-from common import MenuView, HomeView, LicenseView
+from .common import MenuView, HomeView, LicenseView  # noqa
