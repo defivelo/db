e1a09dd2b82c19a3d5856d563b85c317242f1cad
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2020-05-14 14:49:57 +0200

    DEFIVELO-76 Test that days are reset to day=1

--- apps/salary/tests/test_monthlycantonalvalidations.py
+++ apps/salary/tests/test_monthlycantonalvalidations.py
@@ -8,8 +8,10 @@ from .factories import MonthlyCantonalValidationFactory
 
 class MCVTestCase(TestCase):
     def test_no_two_identical_month_canton(self):
-        today = datetime.datetime.today()
-        MonthlyCantonalValidationFactory(canton="VD", date=today)
+        today = datetime.datetime.today().replace(day=4)
+        mcv = MonthlyCantonalValidationFactory(canton="VD", date=today)
+        # Day is reset to start of the month; day=1
+        self.assertEqual(mcv.date.day, 1, mcv)
 
         with self.assertRaises(IntegrityError):
-            MonthlyCantonalValidationFactory(canton="VD", date=today)
+            MonthlyCantonalValidationFactory(canton="VD", date=today.replace(day=8))
