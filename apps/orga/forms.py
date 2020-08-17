6838aaa6c1edabc6ceec7477b87bb293158360e6
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2020-08-17 10:25:32 +0200

    DEFIVELO-190 Orga: set the address_canton initial and required
    As it can be disabled, doing this removes the possibility for CdPs to create canton-less orgas

--- apps/orga/forms.py
+++ apps/orga/forms.py
@@ -33,12 +33,14 @@ from .models import Organization
 class OrganizationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         cantons = kwargs.pop("cantons", None)
-        super(OrganizationForm, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         if cantons:
             # Only permit edition within the allowed cantons
             choices = self.fields["address_canton"].choices
             choices = ((k, v) for (k, v) in choices if k in cantons)
             self.fields["address_canton"].choices = choices
+            self.fields["address_canton"].initial = cantons[0]
+            self.fields["address_canton"].required = True
 
     address_canton = forms.ChoiceField(
         label=_("Canton"),
