00fe378c120b30e3f85380964ad76926561f78f2
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2020-10-28 15:45:21 +0100

    DEFIVELO-98 Disable minDate too, to avoid year skips

--- apps/challenge/forms/session.py
+++ apps/challenge/forms/session.py
@@ -45,16 +45,16 @@ class SessionForm(forms.ModelForm):
                 address_canton__in=self.season.cantons
             )
             self.fields["orga"].queryset = qs
-        try:
-            self.fields["day"].widget.options["minDate"] = self.season.begin.strftime(
-                "%Y-%m-%d"
-            )
-            # Disable maxDate - DEFIVELO-98
-            # self.fields["day"].widget.options["maxDate"] = self.season.end.strftime(
-            #     "%Y-%m-%d"
-            # )
-        except Exception:
-            pass
+        # Disable minDate and maxDate - DEFIVELO-98
+        # try:
+        #     self.fields["day"].widget.options["minDate"] = self.season.begin.strftime(
+        #         "%Y-%m-%d"
+        #     )
+        #     self.fields["day"].widget.options["maxDate"] = self.season.end.strftime(
+        #         "%Y-%m-%d"
+        #     )
+        # except Exception:
+        #     pass
 
     day = SwissDateField(label=_("Date"))
     begin = SwissTimeField(label=_("Début"), required=False)
