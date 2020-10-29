cebf687b0d1909653e177201713b3b8d2fd5ea2a
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2020-10-29 14:12:59 +0100

    DEFIVELO-201 Teste que les chargés de projet peuvent rendre les timesheets ignorées

--- apps/salary/tests/test_timesheets_view.py
+++ apps/salary/tests/test_timesheets_view.py
@@ -306,6 +306,58 @@ def test_state_manager_can_validate_timesheet(db):
     assert Timesheet.objects.count() == 1 and Timesheet.objects.first().validated_at
 
 
+def test_state_manager_can_set_timesheet_to_ignore(db):
+    client = StateManagerAuthClient()
+    managed_cantons = user_cantons(client.user)
+    actor = UserFactory(
+        first_name="Maurice",
+        last_name="Moss",
+        profile__affiliation_canton=managed_cantons[0],
+    )
+    SeasonFactory(
+        cantons=[managed_cantons[0]],
+        year=2019,
+        season=DV_SEASON_SPRING,
+    )
+    QualificationFactory(
+        actor=actor,
+        session=SessionFactory(
+            day=datetime.date(2019, 4, 12),
+            orga=OrganizationFactory(address_canton=managed_cantons[0]),
+        ),
+    )
+
+    datas = {
+        "form-TOTAL_FORMS": "1",
+        "form-INITIAL_FORMS": "1",
+        "form-MIN_NUM_FORMS": "0",
+        "form-MAX_NUM_FORMS": "0",
+        "form-0-date": "2019-04-12",
+        "form-0-time_helper": "4.5",
+        "form-0-actor_count": "0",
+        "form-0-leader_count": "0",
+        "form-0-overtime": "0.25",
+        "form-0-traveltime": "0.25",
+        "form-0-comments": "Comment",
+        "form-0-ignore": True,
+    }
+
+    client.post(
+        reverse(
+            "salary:user-timesheets",
+            kwargs={"year": 2019, "month": 4, "pk": actor.pk},
+        ),
+        datas,
+    )
+    assert Timesheet.objects.count() == 1
+    ts = Timesheet.objects.first()
+    assert ts.ignore
+    assert ts.get_total_amount_helper() == 0
+    assert ts.get_total_amount_actor() == 0
+    assert ts.get_total_amount_leader() == 0
+    assert ts.get_total_amount() == 0
+
+
 def test_helper_cannot_timesheet_if_he_has_not_work(db):
     client = AuthClient()
     datas = {
