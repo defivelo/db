e1f5937a34fae2f1cbda651d5853085815262113
Renato Moor <renatomoor1@gmail.com>
2021-01-12 09:51:00 +0100

    DEFIVELO-215 Remove fixturize command

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,6 @@ if [ "$INITIAL" = "1" ]; then
     if [ ! -e "requirements/test.txt" ]; then
         pip-compile requirements/test.in
     fi
-
-    # Wait for the db server to be ready, then run the fixturize command
-    while ! (echo > /dev/tcp/db/5432) >/dev/null 2>&1; do echo -n '.'; sleep 1; done;
-    echo "Running fixturize..."
-    ./manage.py fixturize -y
 fi
 
 exec "${@}"
