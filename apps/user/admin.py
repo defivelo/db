ef63bc1dae3347f960f1b6a752bcc3c744ee3d0e
Cornut Matthieu <matthieu.cornut@liip.ch>
2022-08-22 17:28:21 +0200

    DEFIVELO-246 Enforce limit to 1 primary email address

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -13,8 +13,11 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+from django import forms
 from django.contrib import admin
+
+from allauth.account.admin import EmailAddressAdmin as BaseEmailAddressAdmin
+from allauth.account.models import EmailAddress
 
 from .models import UserManagedState, UserProfile
 
@@ -25,3 +28,26 @@ class AdminUserProfile(admin.ModelAdmin):
 
 admin.site.register(UserProfile, AdminUserProfile)
 admin.site.register(UserManagedState)
+
+
+class EmailAddressAdminForm(forms.ModelForm):
+    class Meta:
+        model = EmailAddress
+        fields = "__all__"
+
+    def save(self, commit=True):
+        if self.cleaned_data["primary"] is True:
+            EmailAddress.objects.filter(user=self.instance.user, primary=True,).exclude(
+                id=self.instance.id
+            ).update(primary=False)
+
+        return super().save(commit)
+
+
+class EmailAddressAdmin(BaseEmailAddressAdmin):
+    list_filter = ("primary",)
+    form = EmailAddressAdminForm
+
+
+admin.site.unregister(EmailAddress)
+admin.site.register(EmailAddress, EmailAddressAdmin)
